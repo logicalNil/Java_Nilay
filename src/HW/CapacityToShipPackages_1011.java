118a45b0ca2ac8b872de6ba3f3ac8c6c90afb932
nilay <nilayranjan99@gmail.com>
2023-02-22 17:37:27 +0530

    1011

---
+++ src/HW/CapacityToShipPackages_1011.java
@@ -0,0 +1,37 @@
+package HW;
+
+public class CapacityToShipPackages_1011 {
+    public static int shipWithinDays(int[] weights, int D) {
+        int left = 0;
+        int right = 0;
+        for (int weight : weights) {
+            left = Math.max(left, weight);
+            right += weight;
+        }
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int need = 1;
+            int cur = 0;
+            for (int weight : weights) {
+                if (cur + weight > mid) {
+                    need++;
+                    cur = 0;
+                }
+                cur += weight;
+            }
+            if (need > D) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] weights = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10 , 11, 12, 13, 14, 15};
+        int D = 5;
+        int leastCapacity = shipWithinDays(weights, D);
+        System.out.println("The least capacity of the ship is " + leastCapacity);
+    }
+}
