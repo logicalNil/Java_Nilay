a90aff35999c9c6c9cb81db68d47c4848578b033
nilay <nilayranjan99@gmail.com>
2023-02-25 19:06:49 +0530

    121 Leet

--- src/HW/BestTimetoBuyandSellStock_121.java
+++ src/HW/BestTimetoBuyandSellStock_121.java
@@ -14,5 +14,18 @@ public class BestTimetoBuyandSellStock_121 {
         }
         return maxProfit;
     }
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        BestTimetoBuyandSellStock_121 obj1 = new BestTimetoBuyandSellStock_121();
+        int prices;
+        System.out.println("Enter the number of prices: ");
+        prices = obj.nextInt();
+        int[] pricesArray = new int[prices];
 
+        System.out.println("Enter the prices: ");
+        for (int i = 0; i < prices; i++) {
+            pricesArray[i] = obj.nextInt();
+        }
+        System.out.println("The maximum profit is: " + obj1.maxProfit(pricesArray));
+    }
 }
