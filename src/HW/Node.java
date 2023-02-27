76ee6baf1f059ea29c170c457012c499b6e6b601
nilay <nilayranjan99@gmail.com>
2023-02-27 19:59:19 +0530

    #427

---
+++ src/HW/Node.java
@@ -0,0 +1,13 @@
+package HW;
+
+public class Node {
+    public boolean isLeaf;
+    public boolean val;
+      public Node topLeft;
+    public Node bottomLeft;
+    public Node bottomRight;
+    public Node topRight;
+
+    public Node(boolean b, boolean b1) {
+    }
+}
