7187271dbcefc3ad18c8ac7f9390e907bb27d43d
nilay <nilayranjan99@gmail.com>
2023-03-01 19:05:43 +0530

    #652

--- src/HW/FindDuplicateSubtrees_652.java
+++ src/HW/FindDuplicateSubtrees_652.java
@@ -7,20 +7,5 @@ import java.util.List;
 import java.util.Map;
 
 public class FindDuplicateSubtrees_652 {
-    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        List<TreeNode> ans = new ArrayList<>();
-        Map<String, Integer> count = new HashMap<>();
-        encode(root, count, ans);
-        return ans;
-    }
-
-    private String encode(TreeNode root, Map<String, Integer> count, List<TreeNode> ans) {
-        if (root == null) return null;
-        
-        final String encoded = root.val + "#" + encode(root.left, count, ans) + "#" + encode(root.right, count, ans);
-        Integer merge = count.merge(encoded, 1, Integer::sum);
-        if(count.get(encoded) == 2) ans.add(root);
-        return encoded;
-    }
 }
 
