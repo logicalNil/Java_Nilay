caf4fa4c3a66e348a62bd90df1ec44a32fc06984
nilay <nilayranjan99@gmail.com>
2023-03-03 12:02:26 +0530

    #28

---
+++ src/HW/IndexofFirstOccurrenceinString_28.java
@@ -0,0 +1,27 @@
+package HW;
+
+public class IndexofFirstOccurrenceinString_28 {
+    public int strStr(String haystack, String needle) {
+        if (needle.length() == 0) return 0;
+        if (haystack.length() == 0) return -1;
+        int i = 0, j = 0;
+        while (i < haystack.length() && j < needle.length()) {
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+            } else {
+                i = i - j + 1;
+                j = 0;
+            }
+        }
+        if (j == needle.length()) return i - j;
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        IndexofFirstOccurrenceinString_28 obj = new IndexofFirstOccurrenceinString_28();
+        String haystack = "hello";
+        String needle = "ll";
+        System.out.println(obj.strStr(haystack, needle));
+    }
+}
