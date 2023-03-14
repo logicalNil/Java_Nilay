443479902f3df2c41fad12408f562f7f24dc421b
nilay <nilayranjan99@gmail.com>
2023-03-14 17:28:42 +0530

    129

---
+++ src/HW/SumRootLeafNumbers_129.java
@@ -0,0 +1,33 @@
+package HW;
+
+import javax.swing.tree.TreeNode;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+
+public class SumRootLeafNumbers_129{
+
+    public int sumNumbers(TreeNode root) {
+        return sumNumbers(root, 0);
+    }
+
+    private int dfs(TreeNode root, int sum) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return sum;
+        }
+        return dfs(root.left, sum) + dfs(root.right, sum);
+    }
+
+    }
+}
