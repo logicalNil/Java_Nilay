7bbc162a4aef8491c6c1e4460ea4a368a7474d00
nilay <nilayranjan99@gmail.com>
2023-03-14 17:27:41 +0530

    109

--- src/ConvertSortedListtoBinarySearchTree_109.java
+++ src/ConvertSortedListtoBinarySearchTree_109.java
@@ -30,21 +30,5 @@ import javax.swing.tree.TreeNode;
  */
 
 public class ConvertSortedListtoBinarySearchTree_109 {
-    public TreeNode sortedListToBST(ListNode head) {
-        if (head == null) return null;
-        if (head.next == null) return new TreeNode(head.val);
-        ListNode slow = head;
-        ListNode fast = head;
-        ListNode pre = null;
-        while (fast != null && fast.next != null) {
-            pre = slow;
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        pre.next = null;
-        TreeNode root = new TreeNode(slow.val);
-        root.left = sortedListToBST(head);
-        root.right = sortedListToBST(slow.next);
-        return root;
-    }
+
 }
