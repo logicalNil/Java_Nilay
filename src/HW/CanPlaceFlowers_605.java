61814919a8834bddad2e049902c3ca73da50e9ca
nilay <nilayranjan99@gmail.com>
2023-03-20 19:12:12 +0530

    605

---
+++ src/HW/CanPlaceFlowers_605.java
@@ -0,0 +1,18 @@
+package HW;
+
+public class CanPlaceFlowers_605 {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                int next = (i == flowerbed.length - 1) ? 0 : flowerbed[i + 1];
+                int prev = (i == 0) ? 0 : flowerbed[i - 1];
+                if (next == 0 && prev == 0) {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+            }
+        }
+        return count >= n;
+    }
+}
