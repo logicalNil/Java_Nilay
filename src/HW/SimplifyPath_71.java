1355b67d59ed25465da686d4e6fcdd830aeaffbd
nilay <nilayranjan99@gmail.com>
2023-04-12 20:56:27 +0530

    71 with Queue

--- src/HW/SimplifyPath_71.java
+++ src/HW/SimplifyPath_71.java
@@ -1,8 +1,10 @@
 package HW;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
-public class SimplifyPath_71 {
+/**public class SimplifyPath_71 {
     public String simplifyPath(String path) {
         Stack<String> s = new Stack<>();
         StringBuilder res = new StringBuilder();
@@ -22,7 +24,24 @@ public class SimplifyPath_71 {
 
         return res.toString();
     }
+**/
 
+public class SimplifyPath_71 {
+    public String simplifyPath(String path){
+        Queue<String> q = new LinkedList<>();
+        String[] p = path.split("/");
+        for(int i=0;i<p.length;i++){
+            if(!q.isEmpty() && p[i].equals("..")) q.poll();
+            else if(!p[i].equals("") && !p[i].equals(".") && !p[i].equals(".."))
+                q.offer(p[i]);
+        }
+        if(q.isEmpty()) return "/";
+        StringBuilder res = new StringBuilder();
+        while(!q.isEmpty()){
+            res.append("/").append(q.poll());
+        }
+        return res.toString();
+    }
     public static void main(String[] args) {
         SimplifyPath_71 sp = new SimplifyPath_71();
         String path = "/a/./b/../../c/";
