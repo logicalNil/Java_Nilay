94165f46852a21d623d0be90e14ed45d85d103bf
nilay <nilayranjan99@gmail.com>
2023-06-14 10:08:41 +0530

    Commit

--- src/Easy/Q226.java
+++ src/Easy/Q226.java
@@ -3,25 +3,46 @@ package Easy;
 import HW.List;
 
 public class Q226 {
-        public List<String> summaryRanges(int[] nums) {
-      List<String> list=new ArrayList();
-      if(nums.length==1){
-          list.add(nums[0]+"");
-          return list;
-      }
-      for(int i=0;i<nums.length;i++){
-          int a=nums[i];
+         public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        if(nums.length == 0) return res;
 
-          while(i+1<nums.length && (nums[i+1]-nums[i])==1){
-              i++;
-          }
-          if(a!=nums[i]){
-              list.add(a+"->"+nums[i]);
-          }
-          else{
-              list.add(a+"");
-          }
-      }
-        return list;
+        StringBuilder range = new StringBuilder();
+
+        int min = nums[0];
+        int max = nums[0];
+
+        int prev = nums[0];
+
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] != prev + 1) {
+                addRange(range, res, min, max);
+
+                min = nums[i];
+                max = nums[i];
+            }
+
+            min = Math.min(min, nums[i]);
+            max = Math.max(min, nums[i]);
+
+            prev = nums[i];
+        }
+
+        addRange(range, res, min, max);
+
+        return res;
+    }
+
+    private void addRange(StringBuilder range, List<String> res,
+                            int min, int max) {
+        range.append(Integer.toString(min));
+
+        if(min != max) {
+            range.append("->");
+            range.append(Integer.toString(max));
+        }
+
+        res.add(range.toString());
+        range.setLength(0);
     }
 }
